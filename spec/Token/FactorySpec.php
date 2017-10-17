08bdd14e2d2068d40a6e7bd778f96a434fc81aae
Luciano Longo <luciano.longo@gmail.com>
2017-10-16 23:26:09 -0300

    Add Token factory implementation
    Since the JWT library's Builder class keeps it's state after the token
    has been built, I cannot use dependency injection, thus, I can't double
    it for the tests, making them a bit ugly and "integrationy".

---
+++ spec/Token/FactorySpec.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace spec\IgnisLabs\HotJot\Token;
+
+use IgnisLabs\HotJot\Contracts\Token\Token;
+use IgnisLabs\HotJot\Token\Factory;
+use Lcobucci\JWT\Signer;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class FactorySpec extends ObjectBehavior
+{
+    function let(Signer $signer)
+    {
+        $key = 'signing key';
+        $this->beConstructedWith($signer, $key);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Factory::class);
+    }
+
+    function it_should_create_a_new_token()
+    {
+        $claims = ['jti' => 'token id'];
+        $headers = ['foo' => 'bar'];
+        /** @var Token $token */
+        $token = $this->create($claims, $headers);
+        $token->shouldBeAnInstanceOf(Token::class);
+        $token->id()->shouldBe('token id');
+        $token->getHeader('foo')->shouldBe('bar');
+    }
+}
---
+++ src/Token/Factory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace IgnisLabs\HotJot\Token;
+
+use IgnisLabs\HotJot\Token;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer;
+use IgnisLabs\HotJot\Contracts\Token\Factory as FactoryContract;
+use IgnisLabs\HotJot\Contracts\Token\Token as TokenContract;
+
+class Factory implements FactoryContract {
+
+    /**
+     * @var Signer
+     */
+    private $signer;
+
+    /**
+     * @var string
+     */
+    private $key;
+
+    /**
+     * Factory constructor.
+     * @param Signer $signer
+     * @param string $key
+     */
+    public function __construct(Signer $signer, string $key) {
+        $this->signer = $signer;
+        $this->key = $key;
+    }
+
+    /**
+     * Create token
+     * @param array $claims
+     * @param array $headers
+     * @return TokenContract
+     */
+    public function create(array $claims, array $headers = []) : TokenContract {
+        $builder = new Builder();
+
+        foreach ($claims as $name => $value) {
+            $builder->set($name, $value);
+        }
+
+        foreach ($headers as $name => $value) {
+            $builder->setHeader($name, $value);
+        }
+
+        return new Token($builder->sign($this->signer, $this->key)->getToken());
+    }
+}

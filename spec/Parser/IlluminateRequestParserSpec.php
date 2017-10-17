527744fe7b018d0bdb7d514c435a67502601e1eb
Luciano Longo <luciano.longo@gmail.com>
2017-10-16 22:44:50 -0300

    Add Laravel request parser

---
+++ spec/Parser/IlluminateRequestParserSpec.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace spec\IgnisLabs\HotJot\Parser;
+
+use IgnisLabs\HotJot\Contracts\Token\Token;
+use IgnisLabs\HotJot\Exceptions\BearerTokenNotFound;
+use IgnisLabs\HotJot\Parser\IlluminateRequestParser;
+use Illuminate\Http\Request;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Token as LcobucciToken;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class IlluminateRequestParserSpec extends ObjectBehavior
+{
+    function let(Request $request, Parser $parser)
+    {
+        $this->beConstructedWith($request, $parser);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(IlluminateRequestParser::class);
+    }
+
+    function it_should_parse_token_from_request(Request $request, Parser $parser, LcobucciToken $token)
+    {
+        $request->bearerToken()->willReturn('a.valid.token');
+        $parser->parse('a.valid.token')->shouldBeCalled()->willReturn($token);
+        $this->parse()->shouldBeAnInstanceOf(Token::class);
+    }
+
+    function it_should_fail_if_authorization_header_has_no_bearer_token(Request $request)
+    {
+        $request->bearerToken()->willReturn();
+        $this->shouldThrow(BearerTokenNotFound::class)->during('parse');
+    }
+}
---
+++ src/Parser/IlluminateRequestParser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace IgnisLabs\HotJot\Parser;
+
+use IgnisLabs\HotJot\Exceptions\BearerTokenNotFound;
+use Illuminate\Http\Request;
+use Lcobucci\JWT\Parser;
+use IgnisLabs\HotJot\Token;
+use IgnisLabs\HotJot\Contracts\RequestParser;
+use IgnisLabs\HotJot\Contracts\Token\Token as TokenContract;
+
+class IlluminateRequestParser implements RequestParser {
+
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * @var Parser
+     */
+    private $parser;
+
+    /**
+     * IlluminateRequestParser constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request, Parser $parser) {
+        $this->request = $request;
+        $this->parser = $parser;
+    }
+
+    /**
+     * Parse token from current request
+     * @return TokenContract
+     */
+    public function parse() : TokenContract {
+        $jwt = $this->request->bearerToken();
+
+        if (!$jwt) {
+            throw new BearerTokenNotFound;
+        }
+
+        return new Token($this->parser->parse($jwt));
+    }
+}

940593a7d435e0f41d1cc53806a69a9674e94b20
Luciano Longo <luciano.longo@gmail.com>
2017-11-15 23:21:34 -0300

    Fix exception parameters

--- src/Exceptions/Validation/TokenCannotBeRefreshedException.php
+++ src/Exceptions/Validation/TokenCannotBeRefreshedException.php
@@ -8,6 +8,6 @@ use Throwable;
 class TokenCannotBeRefreshedException extends ValidationException {
 
     public function __construct($message = "Token cannot be refreshed", $code = 0, Throwable $previous = null) {
-        parent::__construct($message, $code, $previous);
+        parent::__construct('iat', $message, $code, $previous);
     }
 }
